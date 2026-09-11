32d18aec8355874d5e0767fae6632cf356fcc719
Yashwant Chauhan <yashwantchauhan427@gamil.com>
2026-09-11 12:22:29 +0530

    Generate Random Password

---
+++ Script.js
@@ -0,0 +1,25 @@
+const passwordBox=document.getElementById("password");
+const lenght=12;
+const upperCase="ABCDEFGHIJKLMNOPQSTUVWXYZ";
+const lowerCase="abcdefghijklmnopqstuvwxyz";
+const number="0123456789";
+const symbol="@#$%^*()_><?~+!";
+
+const allCharts=upperCase+lowerCase+number+symbol;
+
+function createPassword(){
+    let password="";
+    password+=upperCase[Math.floor(Math.random()*upperCase.length)];
+    password+=lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    password+=number[Math.floor(Math.random()*number.length)];
+    password+=symbol[Math.floor(Math.random()*symbol.length)];
+
+    while(lenght > password.length){
+       password+=allCharts[Math.floor(Math.random()*allCharts.length)];      
+    }
+    passwordBox.value=password;
+}
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
